d07a05d589e387bad8ed11c52fdf24c70b4221e4
Richard <richard232manansala@gmail.com>
2026-08-31 11:12:17 +0800

    feat: added basic youtube video playing

--- resources/js/pages/party/show.tsx
+++ resources/js/pages/party/show.tsx
@@ -1,4 +1,6 @@
 import {Button} from "@/components/ui/button";
+import ReactPlayer from 'react-player'
+import {useEffect, useState} from "react";
 
 type Props = {isAuthorized: false} | { isAuthorized: true;
     party: {
@@ -14,6 +16,8 @@ type Props = {isAuthorized: false} | { isAuthorized: true;
 
 export default function ShowPartyPage(props: Props) {
 
+    const [currentVideo, setCurrentVideo] = useState(0)
+
 
     if(!props.isAuthorized) {
         return <div className="flex flex-col items-center justify-center flex-1 ">
@@ -28,8 +32,31 @@ export default function ShowPartyPage(props: Props) {
         <div className="container mx-auto max-sm:px-3 py-4 pt-6 flex flex-col gap-4">
             <h2 className="text-2xl font-bold tracking-tight">{party.name}</h2>
             <hr/>
-            <div className="w-full max-w-2xl bg-gray-400  aspect-video mx-auto">
-            </div>
+            {
+                songs.length ? (
+                    <ReactPlayer src={songs[currentVideo].uri} className="aspect-video w-full mx-auto"
+                    autoPlay
+                     controls
+
+                     onEnded={() => {
+                         console.log("ended")
+                         if(currentVideo +1 < songs.length) {
+                             setCurrentVideo(v => v+1);
+                         }
+                     }}
+                     config={{
+                         youtube: {
+                             fs: 1,
+                             start: 0,
+                         }
+                     }}>
+
+                    </ReactPlayer>
+                ) : (
+                    <div className="w-full max-w-2xl bg-gray-400  aspect-video mx-auto">
+                    </div>
+                )
+            }
             <div className="flex flex-col w-full">
                 <h3 className="text-lg ">Song Title Here</h3>
                 <div className="flex gap-2 w-full justify-center">

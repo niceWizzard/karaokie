c50af4cda276ce6635c6dc8f139daf5eb84a88b1
Richard <richard232manansala@gmail.com>
2026-09-02 14:26:40 +0800

    refactor: add desktop mode view

--- resources/js/pages/party/show.tsx
+++ resources/js/pages/party/show.tsx
@@ -69,54 +69,59 @@ export default function ShowPartyPage(props: Props) {
     }
 
     return (
-        <div className="container mx-auto max-sm:px-3 py-4 pt-6 flex flex-col gap-4">
-            <h2 className="text-2xl font-bold tracking-tight">{party.name}</h2>
-            <hr/>
-            {
-                songs.length ? (
-                    <ReactPlayer src={songs[currentVideo].uri} className="w-full aspect-video max-w-5xl mx-auto"
-                     width="100%"
-                     height="100%"
-                    autoPlay
-                     playing={!isPaused}
-                     controls
-                     onEnded={onNext}
-                     config={{
-                         youtube: {
-                             start: 0,
-                         }
-                     }}>
+        <div className="container mx-auto max-sm:px-3 py-4 pt-6 flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col flex-1">
+                <h2 className="text-2xl font-bold tracking-tight">{party.name}</h2>
+                <hr/>
+                <div className="aspect-video">
+                    {
+                        songs.length ? (
+                            <ReactPlayer src={songs[currentVideo].uri} className="w-full aspect-video max-w-5xl  mx-auto"
+                                         width="100%"
+                                         height="100%"
+                                         autoPlay
+                                         playing={!isPaused}
+                                         controls
+                                         onEnded={onNext}
+                                         config={{
+                                             youtube: {
+                                                 start: 0,
+                                             }
+                                         }}>
 
-                    </ReactPlayer>
-                ) : (
-                    <div className="w-full max-w-2xl bg-gray-400  aspect-video mx-auto">
-                    </div>
-                )
-            }
-            <div className="flex flex-col w-full">
-                <h3 className="text-lg ">{currentSong?.title ?? "Song"}</h3>
-                <div className="flex gap-2 w-full justify-center">
-                    <Button onClick={onPrevious}>Previous</Button>
-                    <Button onClick={() => setIsPaused(v => !v)}>{isPaused ? 'Play' : 'Pause'}</Button>
-                    <Button onClick={onNext}>Next</Button>
+                            </ReactPlayer>
+                        ) : (
+                            <div className="w-full max-w-2xl bg-gray-400  aspect-video mx-auto">
+                            </div>
+                        )
+                    }
                 </div>
+                <div className="flex flex-col w-full flex-1">
+                    <h3 className="text-lg ">{currentSong?.title ?? "Song"}</h3>
+                    <div className="flex gap-2 w-full justify-center">
+                        <Button onClick={onPrevious}>Previous</Button>
+                        <Button onClick={() => setIsPaused(v => !v)}>{isPaused ? 'Play' : 'Pause'}</Button>
+                        <Button onClick={onNext}>Next</Button>
+                    </div>
+                </div>
+                <hr/>
             </div>
-            <hr/>
-            <h3 className="text-lg">Queue</h3>
             <div className="flex flex-col gap-2 items-center">
-                {
-                    songs.map(song => (
-                        <div key={`guest-song-${song.id}`}
-                             className={
-                                 `flex gap-3 border p-4 w-full min-w-sm rounded-lg bg-foreground/5 shadow ${song.id === party.current_song_id ? 'border-2 border-primary' : ''}`
-                             }
-                        >
-                            <img
-                                src={song.thumbnail}
-                                alt={'Thumbnail of video: '+song.title}
-                            />
-                            <div className="flex flex-col flex-1">
-                                {
+                <h3 className="text-lg">Queue</h3>
+                <div className="flex flex-col gap-2 items-center">
+                    {
+                        songs.map(song => (
+                            <div key={`guest-song-${song.id}`}
+                                 className={
+                                     `flex gap-3 border p-4 w-full min-w-sm rounded-lg bg-foreground/5 shadow ${song.id === party.current_song_id ? 'border-2 border-primary' : ''}`
+                                 }
+                            >
+                                <img
+                                    src={song.thumbnail}
+                                    alt={'Thumbnail of video: '+song.title}
+                                />
+                                <div className="flex flex-col flex-1">
+                                    {
                                         <DropdownMenu>
                                             <DropdownMenuTrigger asChild>
                                                 <Button variant="ghost" size="icon" className="h-8 w-8 self-end">
@@ -140,21 +145,22 @@ export default function ShowPartyPage(props: Props) {
                                                 </DropdownMenuItem>
                                             </DropdownMenuContent>
                                         </DropdownMenu>
-                                }
-                                <div className="flex justify-between gap-4">
-                                    <h2 className="font-medium text-sm md:text-base" title={song.title}>
-                                        {song.title.length > 32 ? `${song.title.slice(0, 32).trim()}...` : song.title}
-                                    </h2>
-                                    <span className="shrink-0 text-xs">#{song.queue_order}</span>
-                                </div>
-                                <span className="text-sm">{formatIsoDuration(song.duration)}</span>
-                                <span className="text-xs md:text-sm font-light text-end flex-1 flex justify-end items-end">
+                                    }
+                                    <div className="flex justify-between gap-4">
+                                        <h2 className="font-medium text-sm md:text-base" title={song.title}>
+                                            {song.title.length > 32 ? `${song.title.slice(0, 32).trim()}...` : song.title}
+                                        </h2>
+                                        <span className="shrink-0 text-xs">#{song.queue_order}</span>
+                                    </div>
+                                    <span className="text-sm">{formatIsoDuration(song.duration)}</span>
+                                    <span className="text-xs md:text-sm font-light text-end flex-1 flex justify-end items-end">
                                     Queued by {song.guest.name}
                                 </span>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
